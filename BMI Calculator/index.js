611b55f53d5753b8d4e6f140d9a575ed825ec0c5
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-06-28 22:44:15 +0530

    init

---
+++ BMI Calculator/index.js
@@ -0,0 +1,11 @@
+const btnEl= document.getElementById("btn");
+
+
+function calculateBMI(){
+    const heightValue = document.getElementById("height").value
+    const weightValue = document.getElementById("weight").value
+
+    
+}
+
+btnEl.addEventListener("click", calculateBMI)
