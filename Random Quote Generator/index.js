92e080095b427427c7d8a8058f76817a6392e283
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-07-02 16:47:16 +0530

    init

---
+++ Random Quote Generator/index.js
@@ -0,0 +1,16 @@
+const btnEl = document.getElementById("btn")
+const quoteEl = document.getElementById("quote")
+const authorEl = document.getElementById("author")
+const apiURL= "https://api.quotable.io/random"
+
+async function getQuote(){
+    const response = await fetch(apiURL)
+    const data = await response.json()
+    const quoteContent = data.content
+    const quoteAuthor = data.author
+    quoteEl.innerText = quoteContent
+    authorEl.innerText = "~ " + quoteAuthor
+    
+}
+
+btnEl.addEventListener("click", getQuote)
