978237de3cf871acb9196fc1f2c15a1cd253ed06
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-07-02 16:52:25 +0530

    init

--- Random Quote Generator/index.js
+++ Random Quote Generator/index.js
@@ -4,12 +4,19 @@ const authorEl = document.getElementById("author")
 const apiURL= "https://api.quotable.io/random"
 
 async function getQuote(){
-    const response = await fetch(apiURL)
+    try {
+        const response = await fetch(apiURL)
     const data = await response.json()
     const quoteContent = data.content
     const quoteAuthor = data.author
     quoteEl.innerText = quoteContent
     authorEl.innerText = "~ " + quoteAuthor
+    console.log(data);
+    } catch (error) {
+        console.log(error);
+        quoteEl.innerText = "An error happened, try again later"
+        authorEl.innerText = "An error happened"
+    }
     
 }
 
