4ea2bc2bdec3f93cf9fae1d3074fabac6422d348
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-08-01 12:24:03 +0530

    init

--- Notes App/index.js
+++ Notes App/index.js
@@ -1,13 +1,33 @@
 const btnEl = document.getElementById("btn")
 
+function createNoteEl(id,content){
+    const element = document.createElement("textarea")
+    element.classList.add("note")
+    element.placeholder = "Empty Note"
+    element.value = content
 
+    element.addEventListener("dblclick", ()=>{
+        const warning = confirm("Do you want to delete this note?")
+        if(warning){
+            deleteNote(id, element)
+        }
+    })
+
+    element.addEventListener("input",()=>{
+        updateNote(id, element.value)
+    })
+}
+
+function deleteNote(){
+
+}
 
 function addNote(){
     const noteObj = {
         id: Math.floor(Math.random() * 100000),
         content: "",
     };
-    console.log(noteObj);
+    const noteEl = createNoteEl(noteObj.id, noteObj.content)
 }
 
 btnEl.addEventListener("click", addNote)
