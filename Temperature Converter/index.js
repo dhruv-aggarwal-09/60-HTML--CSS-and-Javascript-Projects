91ac9f4466b140f4df6b784971c38b49363c7ad7
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-08-02 15:42:33 +0530

    init

---
+++ Temperature Converter/index.js
@@ -0,0 +1,9 @@
+const celsiusEl= document.getElementById("celsius")
+const fahrenheitEl= document.getElementById("fahrenheit")
+const kelvinEl= document.getElementById("kelvin")
+
+
+function computeTemp(event){
+    const currentValue = +event.target.value
+    
+}
