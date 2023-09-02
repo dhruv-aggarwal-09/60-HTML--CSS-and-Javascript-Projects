aac2c11867f5893a04348c28620d37073be34f90
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-09-03 00:23:22 +0530

    init

--- Photo Gallery/index.js
+++ Photo Gallery/index.js
@@ -3,7 +3,10 @@ const btnEl= document.getElementById("btn");
 
 function fetchImage(){
     const inputValue = document.getElementById("input").value
-    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1`)
+    fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=4AZ-bGv3pcsa8g1rSbC9Bc93CZPHsdqF6DPK_n1eQfM`).then((res)=>res.json().then((data)=>{
+        console.log(data);
+    })
+    );
 };
 
 btnEl.addEventListener("click", fetchImage())
