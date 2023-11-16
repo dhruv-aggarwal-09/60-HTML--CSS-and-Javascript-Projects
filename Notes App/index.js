697eb77ace9d2f4a6e23060cfe17b84e88542262
Dhruv Aggarwal <aggarwald716@gmail.com>
2023-11-17 00:11:35 +0530

    init

--- Notes App/index.js
+++ Notes App/index.js
@@ -1,63 +1,64 @@
-const btnEl = document.getElementById("btn")
-const appEl = document.getElementById("app")
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
 
-getNotes().forEach((note)=>{
-    const noteEl = createNoteEl(note.id, note.content)
-    appEl.insertBefore(noteEl, btnEl)
-})
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
 
-function createNoteEl(id,content){
-    const element = document.createElement("textarea")
-    element.classList.add("note")
-    element.placeholder = "Empty Note"
-    element.value = content
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
 
-    element.addEventListener("dblclick", ()=>{
-        const warning = confirm("Do you want to delete this note?")
-        if(warning){
-            deleteNote(id, element)
-        }
-    })
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
 
-    element.addEventListener("input",()=>{
-        updateNote(id, element.value)
-    })
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
 
-    return element;
+  return element;
 }
 
-function deleteNote(id, element){
+function deleteNote(id, element) {
     const notes = getNotes().filter((note)=>note.id != id)
     saveNote(notes)
     appEl.removeChild(element)
 }
 
-function updateNote(id, content){
-    const notes = getNotes()
-    const target = notes.filter((note)=>note.id == id)[0];
-    target.content = content;
-    saveNote(notes)
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
 }
 
-function addNote(){
-    const notes = getNotes();
-    const noteObj = {
-        id: Math.floor(Math.random() * 100000),
-        content: "",
-    };
-    const noteEl = createNoteEl(noteObj.id, noteObj.content);
-    appEl.insertBefore(noteEl, btnEl)
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
 
-    notes.push(noteObj)
+  notes.push(noteObj);
 
-    saveNote(notes)
-}
-function saveNote(notes){
-    localStorage.setItem("note-app", JSON.stringify(notes))
+  saveNote(notes);
 }
 
-function getNotes(){
-    return JSON.parse(localStorage.getItem("note-app") || "[]")
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
 }
 
-btnEl.addEventListener("click", addNote)
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
