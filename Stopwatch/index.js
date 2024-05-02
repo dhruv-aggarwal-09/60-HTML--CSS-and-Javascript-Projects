4d98ad78b2924a212a5c65cbe4018f0832429339
Dhruv Aggarwal <aggarwald716@gmail.com>
2024-05-02 17:14:05 +0530

    init

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -26,11 +26,12 @@ function formatTime(elapsedTime){
     const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     return(
         (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
-    ":" +
-    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
-    ":" +
-    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
-    "." +
-    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+        ":" +
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+        ":" +
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+        "." +
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds)
     );
 }
+
