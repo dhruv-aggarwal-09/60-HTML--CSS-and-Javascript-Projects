5a264705328b677930f5883639f799870fc5601c
Dhruv Aggarwal <aggarwald716@gmail.com>
2024-05-04 23:12:21 +0530

    init

---
+++ Pomodoro Timer/index.js
@@ -0,0 +1,15 @@
+const startEl = document.getElementById("start")
+const stopEl = document.getElementById("stop")
+const resetEl = document.getElementById("reset")
+const timerEl = document.getElementById("timer")
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer(){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    timerEl.innerHTML = formattedTime;
+}
